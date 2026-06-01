45ef8e730d9dc2590d541572a713909d60f6fbcf
garibaadmi0008-spec <garibaadmi0008@gmail.com>
2026-06-01 22:00:24 +0530

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 ```javascript
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
 
 const app = express();
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Instagram Downloader Backend Running");
+    res.send("Backend Running Successfully");
 });
 
-app.get("/download", async (req, res) => {
+app.get("/download", (req, res) => {
 
     const reelUrl = req.query.url;
 
@@ -22,29 +21,16 @@ app.get("/download", async (req, res) => {
         });
     }
 
-    try {
-
-        const response = await axios.get(
-            `https://api.codetabs.com/v1/proxy/?quest=${encodeURIComponent(reelUrl)}`
-        );
-
-        res.json({
-            success: true,
-            data: response.data
-        });
-
-    } catch (error) {
-
-        res.json({
-            success: false,
-            message: error.message
-        });
-
-    }
+    res.json({
+        success: true,
+        video: reelUrl
+    });
 
 });
 
-app.listen(3000, () => {
-    console.log("Server Running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 ```
